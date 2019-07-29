ef2ea9fa1d725c92c33725fb32de5266437f5017
Brian Pepple <bdpepple@gmail.com>
2019-07-29 17:15:22 -0400

    Add some initial settings & debug urls

--- desaad/settings.py
+++ desaad/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "desaad.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "desaad",
+        "USER": "bpepple",
+        "PASSWORD": "bamfpassword",
+        "HOST": "",
+        "PORT": "",
     }
 }
 
@@ -112,3 +116,26 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
+if DEBUG:
+    INTERNAL_IPS = ("127.0.0.1", "localhost")
+    MIDDLEWARE += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
+
+    INSTALLED_APPS += ("debug_toolbar",)
+
+    DEBUG_TOOLBAR_PANELS = [
+        "debug_toolbar.panels.versions.VersionsPanel",
+        "debug_toolbar.panels.timer.TimerPanel",
+        "debug_toolbar.panels.settings.SettingsPanel",
+        "debug_toolbar.panels.headers.HeadersPanel",
+        "debug_toolbar.panels.request.RequestPanel",
+        "debug_toolbar.panels.sql.SQLPanel",
+        "debug_toolbar.panels.staticfiles.StaticFilesPanel",
+        "debug_toolbar.panels.templates.TemplatesPanel",
+        "debug_toolbar.panels.cache.CachePanel",
+        "debug_toolbar.panels.signals.SignalsPanel",
+        "debug_toolbar.panels.logging.LoggingPanel",
+        "debug_toolbar.panels.redirects.RedirectsPanel",
+    ]
+
+    DEBUG_TOOLBAR_CONFIG = {"INTERCEPT_REDIRECTS": False}
