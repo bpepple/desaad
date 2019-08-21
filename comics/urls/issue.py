4153658d05c5afb3fe9aafb68e6f703e6731f5d3
Brian Pepple <bdpepple@gmail.com>
2019-08-21 17:00:24 -0400

    Initial work on issue detail view

--- comics/urls/issue.py
+++ comics/urls/issue.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
-from comics.views.issue import IssueList
+from comics.views.issue import IssueDetail, IssueList
 
 app_name = "issue"
-urlpatterns = [path("", IssueList.as_view(), name="list")]
+urlpatterns = [
+    path("", IssueList.as_view(), name="list"),
+    path("<slug:slug>/", IssueDetail.as_view(), name="detail"),
+]
--- comics/views/issue.py
+++ comics/views/issue.py
@@ -1,6 +1,7 @@
+from django.db.models import Prefetch
 from django.views.generic import DetailView, ListView
 
-from comics.models import Issue
+from comics.models import Credits, Issue
 
 PAGINATE = 28
 
@@ -9,3 +10,18 @@ class IssueList(ListView):
     model = Issue
     paginate_by = PAGINATE
     queryset = Issue.objects.select_related("series")
+
+
+class IssueDetail(DetailView):
+    model = Issue
+    queryset = Issue.objects.select_related(
+        "series", "series__publisher"
+    ).prefetch_related(
+        Prefetch(
+            "credits_set",
+            queryset=Credits.objects.order_by("creator__name")
+            .distinct("creator__name")
+            .select_related("creator")
+            .prefetch_related("role"),
+        )
+    )
