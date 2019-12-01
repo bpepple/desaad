97dbe76f0ae46fe97cdeac87fd3624c25a40d37d
Brian Pepple <bdpepple@gmail.com>
2019-12-01 17:34:51 -0500

    Add some more tests for the comic importer

--- comics/tests/test_importer_importcomics.py
+++ comics/tests/test_importer_importcomics.py
@@ -1,22 +1,58 @@
 from django.test import TestCase
+from django.utils import timezone
 
 from comics.comicapi.genericmetadata import GenericMetadata
 from comics.importer.importcomics import ComicImporter
-from comics.models import Creator
+from comics.models import Creator, Issue, Publisher, Series
 
 
 class TestComicImporter(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.comic_import = ComicImporter()
-        cls.creator = Creator.objects.create(
-            mid=1, name="John Byrne", slug="john-byrne",
-        )
 
-    # TODO: Add test for the issue slug when the issue already exists in the database.
+        cover_date = timezone.now().date()
+        mod_time = timezone.now()
+
+        cls.publisher = Publisher.objects.create(
+            mid=5, name="DC Comics", slug="dc-comics"
+        )
+        series = Series.objects.create(
+            mid=5,
+            publisher=cls.publisher,
+            name="Superman",
+            slug="superman-2016",
+            year_began="2016",
+        )
+        Issue.objects.create(
+            mid="4321",
+            slug="superman-2016-001",
+            file="/home/b.cbz",
+            mod_ts=mod_time,
+            cover_date=cover_date,
+            number="1",
+            series=series,
+        )
+        Creator.objects.create(mid=1, name="John Byrne", slug="john-byrne")
+
     def test_create_issue_slug(self):
         expected = "batman-2016-005"
         result = self.comic_import.create_issue_slug("batman-2016", 5)
+        self.assertEqual(expected, result)
+
+    def test_create_issue_slug_with_existing_record(self):
+        expected = "superman-2016-001-1"
+        result = self.comic_import.create_issue_slug("superman-2016", 1)
+        self.assertEqual(expected, result)
+
+    def test_create_series_slug(self):
+        expected = "batman-2016"
+        result = self.comic_import.create_series_slug("batman", 2016)
+        self.assertEqual(expected, result)
+
+    def test_create_series_slug_with_existing_record(self):
+        expected = "superman-2016-1"
+        result = self.comic_import.create_series_slug("superman", 2016)
         self.assertEqual(expected, result)
 
     def test_create_creator_slug(self):
@@ -35,3 +71,8 @@ class TestComicImporter(TestCase):
         expected = "7614"
         result = self.comic_import.get_metron_issue_id(meta_data)
         self.assertEqual(expected, result)
+
+    def test_get_publisher_obj_existing(self):
+        result = self.comic_import.get_publisher_obj(self.publisher.mid)
+        self.assertIsNotNone(result)
+        self.assertEqual(result.name, self.publisher.name)
