6f03ce2a4bfd126476b52ad69eea7bdb73f14fbd
Brian Pepple <bdpepple@gmail.com>
2019-12-21 15:46:28 -0500

    Add some signals to delete images

--- comics/apps.py
+++ comics/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 from django.db.models.signals import pre_delete
 
-from comics.signals import pre_delete_image, pre_delete_issue
+from comics.signals import pre_delete_character, pre_delete_image, pre_delete_issue
 
 
 class ComicsConfig(AppConfig):
@@ -11,9 +11,19 @@ class ComicsConfig(AppConfig):
         arc = self.get_model("Arc")
         pre_delete.connect(pre_delete_image, sender=arc, dispatch_uid="pre_delete_arc")
 
+        character = self.get_model("Character")
+        pre_delete.connect(
+            pre_delete_character, sender=character, dispatch_uid="pre_delete_character"
+        )
+
         creator = self.get_model("Creator")
         pre_delete.connect(
             pre_delete_image, sender=creator, dispatch_uid="pre_delete_creator"
+        )
+
+        issue = self.get_model("Issue")
+        pre_delete.connect(
+            pre_delete_issue, sender=issue, dispatch_uid="pre_delete_issue"
         )
 
         publisher = self.get_model("Publisher")
@@ -21,7 +31,7 @@ class ComicsConfig(AppConfig):
             pre_delete_image, sender=publisher, dispatch_uid="pre_delete_publisher"
         )
 
-        issue = self.get_model("Issue")
+        team = self.get_model("Team")
         pre_delete.connect(
-            pre_delete_issue, sender=issue, dispatch_uid="pre_delete_issue"
+            pre_delete_image, sender=team, dispatch_uid="pre_delete_team"
         )
